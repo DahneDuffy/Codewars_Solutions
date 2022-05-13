7de50198fa2e5d4d539921cae88e3887a132d13c
dahne duffy <12duffd@gmail.com>
2022-05-12 23:46:31 -0600

    Codewars 05.12.2022

---
+++ alphabetOrder.js
@@ -0,0 +1,24 @@
+// 6kyu Replace With Alphabet Position 
+
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+// SOlUTION
+
+function alphabetPosition(text) {
+    let trimmedText = text.toLowerCase().replace(/[^a-z]/g,'').split('')
+    
+    return trimmedText.map(char=>{
+      console.log(char)
+      return char.charCodeAt(0)-96
+    }).join(' ')
+  }
